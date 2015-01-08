c189a022b52d424d5bc171f1823ea00accb36b57
Kensuke Numakura <knmkr3gma@gmail.com>
2015-01-08 17:21:54 +0900

    update karabiner setting for US keybord

--- keybind/osx/karabiner-us-import.sh
+++ keybind/osx/karabiner-us-import.sh
@@ -2,16 +2,12 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.jis_enthumble 1
+$cli set remap.jis_commandL2commandL_eisuu 1
 /bin/echo -n .
 $cli set repeat.wait 33
 /bin/echo -n .
 $cli set repeat.initial_wait 300
 /bin/echo -n .
-$cli set remap.commandL2optionL 1
-/bin/echo -n .
-$cli set remap.controlJ2return 1
-/bin/echo -n .
-$cli set remap.jis_kana2optionL_kana 1
+$cli set remap.jis_commandR2commandR_kana 1
 /bin/echo -n .
 /bin/echo
