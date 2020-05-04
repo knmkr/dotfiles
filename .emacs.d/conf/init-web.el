25c154cb94d565d81469fc10fddff17385375080
Kensuke Numakura <knmkr3gma@gmail.com>
2020-05-04 16:27:05 +0900

    disable web-mode

--- .emacs.d/conf/init-web.el
+++ .emacs.d/conf/init-web.el
@@ -2,7 +2,7 @@
 
   ;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   ;; (add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 
   ;; (setq web-mode-markup-indent-offset 2)
   ;; (setq web-mode-code-indent-offset 2)
